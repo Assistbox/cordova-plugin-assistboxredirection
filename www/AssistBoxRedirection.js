f166981226a8ab25e400f1af2eb034cf8f77feec
yildirayyilmaz <yildiray.yilmaz@hotmail.com>
2019-07-30 16:56:25 +0300

    ..

--- www/AssistBoxRedirection.js
+++ www/AssistBoxRedirection.js
@@ -1,8 +1,7 @@
-cordova.define("cordova-plugin-assistboxredirection.AssistBoxRedirection", function(require, exports, module) {
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
+function AssistBoxRedirection() {}
 
-exports.goBackToMainApplication = function (success, error, arg) {
-    exec(success, error, 'AssistBoxRedirection', 'goBackToMainApplication', [arg]);
+AssistBoxRedirection.prototype.goBackToMainApplication = function(successCallback, errorCallback, arg) {
+  exec(successCallback, errorCallback, "AssistBoxRedirection", "goBackToMainApplication", [arg]);
 };
-
-});
+module.exports = new AssistBoxRedirection();
