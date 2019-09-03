51b76c2e74e174f0c261522eb4ca0c5c4b65bd5d
YILDIRAY YILMAZ <yildiray.yilmaz@nayahsoftware.com>
2019-09-03 18:01:44 +0300

    Navigation stack support

--- src/ios/AssistBoxRedirection.m
+++ src/ios/AssistBoxRedirection.m
@@ -8,11 +8,20 @@
     
 }
 
-- (void)goBackToMainApplication:(CDVInvokedUrlCommand*)command
+- (void)goBackToMainApplication:(ASTInvokedUrlCommand*)command
 {
+    NSLog(@"AssistBox Redirection: goBackToMainApplication");
     UIWindow *window = [[UIApplication sharedApplication] keyWindow];
     UINavigationController* navController = (UINavigationController*)window.rootViewController;
-    [navController dismissViewControllerAnimated:YES completion:nil];
+    if(((ASTViewController*)self.viewController).hasNavigationStack){
+        navController.navigationBar.hidden = ((ASTViewController*)self.viewController).isMainAppNavigationBarHidden;
+        NSLog(@"AssistBox Redirection: goBackToMainApplication - calling popViewController");
+        [navController  popViewControllerAnimated:NO];
+    } else {
+        NSLog(@"AssistBox Redirection: goBackToMainApplication - calling dismissViewController");
+        UINavigationController* navController = (UINavigationController*)window.rootViewController;
+        [navController  dismissViewControllerAnimated:NO completion:nil];
+    }
 }
 
 @end
